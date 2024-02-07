1f15978aa1437ed2e4ebc46adf684c6e5192640c
est3banst <estebeeen@outlook.com>
2024-02-07 18:21:54 -0300

    fixed modal

--- src/componentes/Works.jsx
+++ src/componentes/Works.jsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
+
+import {kitchenImages, bedroomImages} from '../componentes/pictures'
+
 import '../assets/Work.css'
-import Kitchen from './Kitchen';
+import '../assets/Gallery.css'
+
+
 
 export default function Works() {
-    
-    const [choice, setChoice] = useState(false);
 
-    const makeChoice = () => {
-        setChoice(!choice)
+    const [activeImg, setActiveImg] = useState(0);
+
+    const [choiceone, setChoice] = useState(false);
+    const [choicetwo, setSecondChoice] = useState(false);
+
+    const openModal = () => {
+        setChoice(!choiceone)
     }
-    const isChoiceOnScreen = choice? 'container-hidden' : 'container-choice' ;
 
+    const openSecondModal = () => {
+        setSecondChoice(!choicetwo)
+    }
+    const isChoiceOnScreen = choiceone || choicetwo? 'container-hidden' : 'container-choice';
+
+    
     return (
         <>
             <section className='container-works'>
@@ -22,12 +35,20 @@ export default function Works() {
             <div className={isChoiceOnScreen}>
            
             <section className='container-choice'>
-                <button onClick={makeChoice}>
-                <svg className='choice-selection' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" 
+                <button onClick={openModal}>
+                <svg className='choice-selection' 
+                    width="24" 
+                    height="24" 
+                    viewBox="0 0 24 24" 
+                    strokeWidth="2" 
                     stroke="currentColor" fill="none" strokeLinecap="round" 
-                    strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M3 12l3 0" /><path d="M12 3l0 3" /><path d="M7.8 7.8l-2.2 -2.2" />
-                    <path d="M16.2 7.8l2.2 -2.2" /><path d="M7.8 16.2l-2.2 2.2" />
+                    strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M3 12l3 0" />
+                    <path d="M12 3l0 3" />
+                    <path d="M7.8 7.8l-2.2 -2.2" />
+                    <path d="M16.2 7.8l2.2 -2.2" />
+                    <path d="M7.8 16.2l-2.2 2.2" />
                     <path d="M12 12l9 3l-4 2l-2 4l-3 -9" />
                 </svg>
                <p>Cocina</p> 
@@ -35,12 +56,20 @@ export default function Works() {
                 <img src="/imgs/cocina/kitchenchoice.jpg" alt="" />
             </section>
             <section className='container-choice'>
-                <button> 
-                <svg className='choice-selection' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" 
+                <button onClick={openSecondModal}> 
+                <svg className='choice-selection' 
+                    width="24" 
+                    height="24" 
+                    viewBox="0 0 24 24" 
+                    strokeWidth="2" 
                     stroke="currentColor" fill="none" strokeLinecap="round" 
-                    strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M3 12l3 0" /><path d="M12 3l0 3" /><path d="M7.8 7.8l-2.2 -2.2" />
-                    <path d="M16.2 7.8l2.2 -2.2" /><path d="M7.8 16.2l-2.2 2.2" />
+                    strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M3 12l3 0" />
+                    <path d="M12 3l0 3" />
+                    <path d="M7.8 7.8l-2.2 -2.2" />
+                    <path d="M16.2 7.8l2.2 -2.2" />
+                    <path d="M7.8 16.2l-2.2 2.2" />
                     <path d="M12 12l9 3l-4 2l-2 4l-3 -9" />
                 </svg>
                 <p>Dormitorio</p>
@@ -48,8 +77,118 @@ export default function Works() {
                 <img src="/imgs/dormitorio/armarioschoice.jpg" alt="" />
             </section>
             </div>
-            {choice && <Kitchen/>
-               }
+            {choiceone && <div id='trabajos' className='container-kitchen-works'>
+            <section className='kitchen-works-section'>
+            
+                
+                {kitchenImages.map((image, activeImg) => {
+                    return (
+                            <div className='container-img-kitchens' key={activeImg}>
+                              
+                            
+                                    <img className='img-kitchens' src={image.imgSrc} alt="" />
+                            <svg className='toclose-icon' 
+                                onClick={openModal} 
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="2" 
+                                stroke="currentColor" 
+                                fill="none" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round">
+                                <path stroke="none" 
+                                d="M0 0h24v24H0z" 
+                                fill="none"/>
+                                <path d="M18 6l-12 12" />
+                                <path d="M6 6l12 12" />
+                            </svg>
+                            <span className='image-control'>
+                            <svg className='arrow-left' 
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="2"
+                                stroke="currentColor" 
+                                fill="none" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M15 6l-6 6l6 6" />
+                                
+                            </svg>
+                            <svg className='arrow-right'
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="2" 
+                                stroke="currentColor" 
+                                fill="none" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M9 6l6 6l-6 6" />
+                            </svg>
+                            </span>
+                                
+                            </div>
+                    )
+                })}
+                
+                
+                </section>
+                </div>
+            }
+            {choicetwo && <div id='trabajos' className='container-kitchen-works'>
+            <section className='kitchen-works-section'>
+            
+                
+                {bedroomImages.map((image, index) => {
+                    return (
+                            <div className='container-img-kitchens' key={index}>
+                            <img className='img-kitchens' src={image.imgSrc} alt="" />
+                            <svg className='toclose-icon' 
+                                onClick={openSecondModal} 
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="2" 
+                                stroke="currentColor" fill="none" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M18 6l-12 12" /><path d="M6 6l12 12" />
+                            </svg>
+                            <span className='image-control'>
+                            <svg className='arrow-left' 
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="2"
+                                stroke="currentColor" fill="none" strokeLinecap="round" 
+                                strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M15 6l-6 6l6 6" />
+                            </svg>
+                            <svg className='arrow-right' 
+                                width="24" 
+                                height="24" 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="2" 
+                                stroke="currentColor" fill="none" strokeLinecap="round" 
+                                strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M9 6l6 6l-6 6" />
+                            </svg>
+                </span>
+                            </div>
+                    )
+                })}
+                
+                
+                </section>
+                </div>
+            }
             
         </>
     )
---
+++ src/componentes/pictures.js
@@ -0,0 +1,38 @@
+import Bajo from '/imgs/cocina/bajomesada.jpg';
+import Mesadau from '/imgs/cocina/bajomesadaenu.jpg';
+import Cocina from '/imgs/cocina/cocinaaereo.jpg';
+import Fullcocina from '/imgs/cocina/cocinacompleta.jpg';
+import Aereo from '/imgs/cocina/cocinaovertop.jpg';
+
+import Placardb from '/imgs/dormitorio/placard5pbl.jpeg'
+import Placardn from '/imgs/dormitorio/placard6pne.jpeg'
+import Placardc from '/imgs/dormitorio/placardcorredizo.jpg'
+import Estantes from '/imgs/dormitorio/camaestantes.jpg'
+
+
+export const kitchenImages = [
+    { id :1,
+    imgSrc: Bajo},
+    { id :2,
+    imgSrc: Mesadau},
+    { id :3,
+    imgSrc: Cocina},
+    { id :4,
+    imgSrc: Fullcocina},
+    { id :5,
+    imgSrc: Aereo},
+]
+
+export const bedroomImages = [
+    {id: 11,
+    imgSrc: Estantes},
+    {id: 12,
+    imgSrc: Placardb},
+    {id: 13,
+    imgSrc: Placardn},
+    {id: 14,
+    imgSrc: Placardc},
+    
+]
+
+
