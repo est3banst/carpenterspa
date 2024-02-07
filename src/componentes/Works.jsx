4ee9f867d99e24e43b1f91295654610b0c09dca3
est3banst <estebeeen@outlook.com>
2024-02-07 01:39:12 -0300

    fixed one works image section

--- src/componentes/Works.jsx
+++ src/componentes/Works.jsx
@@ -1,57 +1,56 @@
 import { useState } from 'react'
 import '../assets/Work.css'
+import Kitchen from './Kitchen';
 
 export default function Works() {
+    
+    const [choice, setChoice] = useState(false);
 
-
-    const [openModal, setModalOpen] = useState(false);
-
-    const modalOpen = openModal ? 'works-images-modal' : 'works-images'
-    const imgsShow = openModal ? 'img-container-modal' : 'img-container'
-    // const galleryImgs = document.querySelectorAll('.works-images')
-
+    const makeChoice = () => {
+        setChoice(!choice)
+    }
+    const isChoiceOnScreen = choice? 'container-hidden' : 'container-choice' ;
 
     return (
         <>
-            <div id='trabajos' className='container-works'>
-                <h2 className='works-heading'>Estos son algunos de nuestros trabajos</h2>
+            <section className='container-works'>
+            <h2 className='works-heading'>Algunos de nuestros trabajos</h2>
                 <p className='para-works'>Cada proyecto se concibe en el diálogo con nuestros clientes al
                     interpretar correctamente la necesidad y trabajar correctamente con los espacios.</p>
-                <section className='works-ul'>
-                <ul>
-                    <li>
-                        <img className={modalOpen} src='/imgs/placard5pbl.jpeg' alt='Aparador de TV esquinero de madera' />
-                        <figcaption>Placard 5 Puertas con modular superior ,8 estantes. Herrajes : con freno, telescópicas y con resorte</figcaption>
-                  
-                    </li>
-                   <li>
-                        <img className={modalOpen} src='/imgs/placard6pne.jpeg' alt='Modulos de cocina, bajo mesada y aéreos' />
-                        <figcaption>Placard 6 puertas con modular superior, 3 estantes. Herrajes : con freno, telescópicas y con resorte</figcaption>
-                  
-                   </li>
-                    <li>
-                        <img className={modalOpen} src='/imgs/cocinaovertop.jpg' alt='Aéreos modulares de cocina imitación madera' />
-                        <figcaption>Modular aéreo en L imitación madera</figcaption>
-             
-                    </li>
-                    <li>
-                        <img className={modalOpen} src='/imgs/cocinacompleta.jpg' alt='Aéreos modulares y bajo mesada en MDF, color amarillo y blanco' />
-                        <figcaption>Modular aéreo de cocina y bajo mesada amarillo y blanco laqueado </figcaption>
-                   
-                    </li>
-                    <li>
-                        <img className={modalOpen} src='/imgs/bajomesada.jpg' alt='Aéreos modulares y bajo mesada en MDF, color amarillo y blanco' />
-                        <figcaption>Modular bajo mesada en L, terminación madera</figcaption>
-                   
-                    </li>
-                    <li>
-                        <img className={modalOpen} src='/imgs/bajomesadaenu.jpg' alt='Aéreos modulares y bajo mesada en MDF, color amarillo y blanco' />
-                        <figcaption>Modular bajo mesada en U, terminación verde manzana laqueado </figcaption>
-                   
-                    </li>
-                </ul>
-                </section>
+              
+            </section>
+            <div className={isChoiceOnScreen}>
+           
+            <section className='container-choice'>
+                <button onClick={makeChoice}>
+                <svg className='choice-selection' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" 
+                    stroke="currentColor" fill="none" strokeLinecap="round" 
+                    strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M3 12l3 0" /><path d="M12 3l0 3" /><path d="M7.8 7.8l-2.2 -2.2" />
+                    <path d="M16.2 7.8l2.2 -2.2" /><path d="M7.8 16.2l-2.2 2.2" />
+                    <path d="M12 12l9 3l-4 2l-2 4l-3 -9" />
+                </svg>
+               <p>Cocina</p> 
+                </button>
+                <img src="/imgs/cocina/kitchenchoice.jpg" alt="" />
+            </section>
+            <section className='container-choice'>
+                <button> 
+                <svg className='choice-selection' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" 
+                    stroke="currentColor" fill="none" strokeLinecap="round" 
+                    strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M3 12l3 0" /><path d="M12 3l0 3" /><path d="M7.8 7.8l-2.2 -2.2" />
+                    <path d="M16.2 7.8l2.2 -2.2" /><path d="M7.8 16.2l-2.2 2.2" />
+                    <path d="M12 12l9 3l-4 2l-2 4l-3 -9" />
+                </svg>
+                <p>Dormitorio</p>
+                 </button>
+                <img src="/imgs/dormitorio/armarioschoice.jpg" alt="" />
+            </section>
             </div>
+            {choice && <Kitchen/>
+               }
+            
         </>
     )
 }
